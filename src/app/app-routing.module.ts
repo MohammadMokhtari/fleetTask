614384a47801b97bd5283696c02f14e8cda22455
Mohammad Mokhtari <dev.mohammadmokhtari@gmailcom>
2022-06-14 14:50:49 +0430

    config Layout Lazy Loading Route Module im appRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{
-  path:'auth',
-  loadChildren:()=>
-  import('./modules/auth/auth.module').then(m=>m.AuthModule)
-}];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren:()=>import('./pages/layout/layout.module').then(m=>m.LayoutModule)
+  },
+  {
+    path:'auth',
+    loadChildren:()=>
+    import('./modules/auth/auth.module').then(m=>m.AuthModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
